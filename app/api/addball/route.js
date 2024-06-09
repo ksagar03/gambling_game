d1199e795c13c9e14504f0ddbd1fcd3e492b62bd
ksagar03 <sagar22krishna03@gmail.com>
2024-06-09 16:05:07 +0530

    Updates: Implemented Rest API for backend communication

--- app/api/addball/route.js
+++ app/api/addball/route.js
@@ -1,3 +1,56 @@
-export async function GET() {
-  return Response.json({ message: "Hello from Next.js!" });
+// import express from express
+// import cors from cors
+
+import { NextResponse } from "next/server";
+
+// const app = express()
+// app.use(cors())
+import { Outcomes } from "@/app/Ball_classes/Outcums";
+const MULTIPLIERS = {
+  0: 16,
+  1: 9,
+  2: 2,
+  3: 1.4,
+  4: 1.4,
+  5: 1.2,
+  6: 1.1,
+  7: 1,
+  8: 0.5,
+  9: 1,
+  10: 1.1,
+  11: 1.2,
+  12: 1.4,
+  13: 1.4,
+  14: 2,
+  15: 9,
+  16: 16,
+};
+const totalDrops = 16;
+
+export async function POST() {
+  let outcome = 0;
+  const pattern = [];
+  for (let i = 0; i < totalDrops; i++) {
+    if (Math.random() > 0.5) {
+      pattern.push("R");
+      outcome++;
+    } else {
+      pattern.push("L");
+    }
+  }
+  const multiplier = MULTIPLIERS[outcome];
+  const possibleOutcomes = Outcomes[outcome];
+
+  return NextResponse.json({
+    point:
+      possibleOutcomes[
+        Math.floor(Math.random() * possibleOutcomes.length || 0)
+      ],
+    multiplier,
+    pattern,
+  });
 }
+
+// export async function GET() {
+//   return NextResponse.json({ message: "Hello from Next.js!" });
+// }
